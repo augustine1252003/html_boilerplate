2385bbc53de6883f3370004f8af7bd6c07df4651
augustine1252003 <augustine1vegeta@gmail.com>
2023-06-12 22:55:31 -0400

    edited to dog

--- script/index.js
+++ script/index.js
@@ -1,1 +1,6 @@
-document.getElementById('tp').innerHTML = 'Hello, World!';
+const btn = document.getElementById('button');
+
+btn.addEventListener('click', function(){
+    document.getElementById('head').innerHTML = 'Random Dog';
+    document.getElementById('para').innerHTML = 'Random Dog did some random thing and made something random happen';
+})
